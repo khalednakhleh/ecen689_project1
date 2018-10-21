2ad44bdca624ef972c751aee81126920c4afedf0
khalednakhleh <khaled.jkn@gmail.com>
2018-10-21 11:24:14 -0500

    finished parsing correctly

--- parsing.py
+++ parsing.py
@@ -10,6 +10,7 @@ file taken from:
 """
 
 import pandas as pd
+import numpy as np
 
 health = pd.read_csv("health_data.csv")
 income = pd.read_csv("income_data.csv")
@@ -19,41 +20,40 @@ print("Initial dimensions for health and income datasets: \n")
 print(health.shape)
 print(income.shape)
 
+###################################################
+income_adjust = income[~((income["COUNTYFIPS"] == 0))]
+income_adjust = income_adjust[~((income_adjust["COUNTYNAME"] == "Kusilvak"))]
+income_adjust = income_adjust[~((income_adjust["COUNTYNAME"] == "Oglala Lakota County"))]
 
-print("\n\n\n--------------------------------")
-print("after parsing dimensions for health and income datasets: \n")
-
-income_adjust = income.drop(income[income.COUNTYFIPS == 0].index)
-income_adjust = income_adjust.drop(income_adjust[income_adjust.COUNTYNAME == "Kusilvak"].index)
-income_adjust = income_adjust.drop(income_adjust[income_adjust.COUNTYNAME == "Oglala Lakota County"].index)
-
-print(income_adjust.shape)
-
-health_adjust = health.drop(health[health.County == "Wade Hampton"].index)
-health_adjust = health_adjust.drop(health_adjust[health_adjust.County == "Kalawao"].index)
+health_adjust = health[~((health["County"] == "Wade Hampton"))]
+health_adjust = health_adjust[~((health["County"] == "Kalawao"))]
 health_adjust = health_adjust[~((health_adjust["County"] == "Shannon") & (health_adjust["State"] == "SD"))]
 health_adjust = health_adjust[~((health_adjust["County"] == "Bedford") & (health_adjust["State"] == "VA")& (health_adjust["PCT_DIABETES_ADULTS08"] == 12.6))]
 
-health_adjust.index += 1
+
+new_index = np.arange(3139)
+income_adjust.index = new_index
+health_adjust.index = new_index
+
+###################################################
+print("\n--------------------------------")
+print("after parsing dimensions for health and income datasets: \n")
+
+print(income_adjust.shape)
 print(health_adjust.shape)
 
-###################################################
+income_adjust.to_csv("‎income_adjusted.csv", index = "Id")
+health_adjust.to_csv("‎health_adjusted.csv", index = "Id")
 
-income_adjust.to_csv("‎income_adjusted.csv")
-health_adjust.to_csv("‎health_adjusted.csv")
- 
 ###################################################
-
 
 # Combining the parsed health and income files into one file
 
-
 combined = pd.concat([health_adjust, income_adjust], axis = 1)
-combined.to_csv("combined.csv")
+combined.to_csv("combined.csv", index = "Id")
 
+print("\nParsed file dimensions: \n")
 print(combined.shape)
-
-
 
 
 
