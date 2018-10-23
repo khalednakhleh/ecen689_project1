efaf7c7af0db3acb313ddf7d64167bc753df1de1
khalednakhleh <khaled.jkn@gmail.com>
2018-10-23 17:59:45 -0500

    minor changes

--- main.py
+++ main.py
@@ -9,16 +9,14 @@ Run the file parsing.py first to obtain the file combined.csv
 """
 
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
 
 def main():
-    health_income = pd.read_csv("combined.csv")
+    
+    health_income = pd.read_csv("combined_2013.csv")
     
     print(health_income.shape)
-    print(health_income.head())
-    
     
     # Grabbing the health data
     diabetes_2008 = health_income.iloc[:, 4]
@@ -27,13 +25,12 @@ def main():
     obesity_2013 = health_income.iloc[:, 7] 
     joint_return = health_income.iloc[:, 22] 
     
-    
+ 
+    # example to help you get started    
 
-    plt.scatter(joint_return, diabetes_2013)
+    plt.scatter(diabetes_2008, joint_return)
     plt.show()
     
-    plt.scatter(obesity_2008, obesity_2013)
-    plt.show()
 
 if __name__ == "__main__":
     main()
